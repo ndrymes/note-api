74c17d7adc711e37f2c1c7c83e275aff54d2add6
Sunmonu Oluwole <sunmonuoluwole@gmail.com>
2019-06-01 07:14:15 +0100

    heroku deploy

--- server.js
+++ server.js
@@ -3,23 +3,24 @@ var bodyParser =require('body-parser')
 var {ObjectID} = require('mongodb')
 var mongoose = require('mongoose');
 var {Todo,User} =require('./model/models')
-var url = 'mongodb://localhost:27017'
+var url = process.env.MONGOLAB_URI ||process.env.MONGOHQ_URL || 'mongodb://localhost:27017'
 mongoose.connect(url);
 
 var app = express();
 app.use(bodyParser.json());
+const port =process.env.PORT || 3000
 
 // POST todo to DB
 app.post('/todos',(req,res) => {
-//  var newTodo = new Todo({
-//    text:req.body.text,
-//    title:req.body.title
-//  })
-//  newTodo.save().then((docs) => {
-//  res.send(docs);
-// }).catch((e) => {
-// res.status(400).send(e)
-// })
+var newTodo = new Todo({
+   text:req.body.text,
+   title:req.body.title
+ })
+ newTodo.save().then((docs) => {
+ res.send(docs);
+}).catch((e) => {
+res.status(400).send(e)
+})
 })
 
 // POST users in // DB
@@ -52,7 +53,7 @@ res.send({todo})
 })
 
 app.listen(3000,() => {
-  console.log('server has started');
+  console.log(`server has started at port ${port}`);
 })
 
 module.exports = {mongoose}
